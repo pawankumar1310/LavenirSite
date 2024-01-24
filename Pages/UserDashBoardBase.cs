b86146916d89c60a03ff54e167793805851a1ae9
Pawan Kumar <Pawankumar@t-a-g.net>
2024-01-24 15:59:09 +0530

    asdsd

--- Pages/UserDashBoardBase.cs
+++ Pages/UserDashBoardBase.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
+using Model;
+using Newtonsoft.Json;
 
 namespace LavenirSite.Pages
 {
@@ -23,14 +25,92 @@ namespace LavenirSite.Pages
         protected string? LyceeProfile { get; set; }
 
 
-        protected string? SelectedCountry { get; set; }
-        protected string? SelectedState { get; set; }
-        protected string? SelectedDistrict { get; set; }
+        protected string? Country { get; set; }
+        protected string? State { get; set; }
+        protected string? District { get; set; }
         protected string? PinCode { get; set; }
         protected string? ReferredBy { get; set; }
         protected List<string> Countries = new List<string>();
         protected List<string> States = new List<string>();
         protected List<string> Districts = new List<string>();
+
+        
+        protected string InputPinCode ;
+        protected string zipCodeID;
+        protected string countryName ;
+ 
+       protected async void DisplayEnteredPinCode(ChangeEventArgs e)
+        {
+        string enteredValue = e.Value.ToString();
+        if (enteredValue.Length == 6)
+        {
+            InputPinCode = enteredValue;
+           await getDetails();
+            Console.WriteLine(InputPinCode);
+ 
+        }
+        else if (enteredValue.Length > 6)
+        {
+            InputPinCode = enteredValue.Substring(0, 6);
+        }
+        }
+ 
+         public async Task getDetails()
+    {
+        try
+        {
+            string pinCode = InputPinCode;
+            var apiUrl2 = $"https://localhost:7000/api/UtilityService/GetZipCodeIDbyZipCode/{560037}";
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                var response2 = await httpClient.GetAsync(apiUrl2);
+                if (response2.IsSuccessStatusCode)
+                {
+                    var jsonResponse = await response2.Content.ReadAsStringAsync();
+                    zipCodeID = jsonResponse.Trim('"');
+                }
+                else
+                {
+                    Console.WriteLine($"Failed to get Zip Code ID. Status code: {response2.StatusCode}");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+     
+    }
+ 
+    private async Task GetPlaceInformation(string zipCodeID)
+{
+    string apiUrlForPlaceInfo = $"https://localhost:7000/api/UserService/getAddress/{zipCodeID}";
+   
+    using (var httpClient = new HttpClient())
+    {
+        httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        var responseinfo = await httpClient.GetAsync($"{apiUrlForPlaceInfo}");
+       
+        if (responseinfo.IsSuccessStatusCode)
+        {
+            var jsonResponse = await responseinfo.Content.ReadAsStringAsync();
+            var placeInformationList = JsonConvert.DeserializeObject<PlaceInformationModel>(jsonResponse);
+ 
+            // Set the values to the corresponding properties in your component
+            Country = placeInformationList.countryName;
+            State = placeInformationList.stateName;
+            District = placeInformationList.cityName;
+ 
+            // Update the UI as needed
+            StateHasChanged();
+        }
+        else
+        {
+            Console.WriteLine($"Failed to get place information. Status code: {responseinfo.StatusCode}");
+        }
+    }
+}
         protected void GetState()
         {
             // Implement logic to fetch states based on the selected country
